e6272b9379fc50808d34392df8d8cf8aa6305722
Salokya Kumar <kumar.salokya007@gmail.com>
2021-11-06 15:04:12 +0530

    final build

--- sudoko.cpp
+++ sudoko.cpp
@@ -1,93 +1,146 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool canPlace(int mat[][9], int i, int j, int n, int number){
+/**
+  *	@desc Checks if the value in the specified cell is valid or not.
+  *	@param i (int) row of the required value
+  *	@param j (int) col of the required value
+  *	@param n (int) size of the board
+  *	@param number (int) the required value
+  *	@return (bool) whether the value is valid or not in the sudoku grid
+*/
+bool canPlace(int mat[][9], int i, int j, int number) {
 
     //row and column check
-    for (int x = 0; x < n; ++x) {
-        if(mat[x][j] == number || mat[i][x] == number){
+    for (int x = 0; x < 9; ++x) {
+        if (mat[x][j] == number || mat[i][x] == number) {
             return false;
         }
     }
 
-    //checking in submatrix
-    int rn = sqrt(n);
-    int sx = (i/rn)*rn;
-    int sy = (j/rn)*rn;
+    //checking in sub-matrix
+    int rn = 3;
+    int sx = (i / rn) * rn;
+    int sy = (j / rn) * rn;
 
-    for (int x = sx; x<sx+rn ; ++x) {
-        for (int y = sy; y <sy+rn; ++y) {
-            if(mat[x][y] == number)
+    for (int x = sx; x < sx + rn; ++x) {
+        for (int y = sy; y < sy + rn; ++y) {
+            if (mat[x][y] == number)
                 return false;
         }
     }
-
     return true;
 }
 
-bool solveSudoku(int mat[][9], int i, int j, int n){
+/**
+  *	@desc Displays the values stored in the grid with designs. We also use
+  *	ANSI colors, using escape sequences, to display the grid.
+  *	@param mat (int [][]) 2d-matrix with input values to be printed
+  *	@return none
+*/
+void displayGrid(int mat[][9]) {
 
-    if(i==n){
-        for (int k = 0; k < n; ++k) {
-            for (int l = 0; l < n; ++l) {
-                cout<<mat[k][l]<<" ";
+    cout << "\n\n++=====================================++";
+    int rowIter, colIter;
+
+    for (rowIter = 0; rowIter < 9; rowIter++) {
+        int cellIter = 1;
+
+        cout << "\n||";
+        for (colIter = 0; colIter < 9; colIter++) {
+            if (cellIter == 3) {
+                cout << " ";
+                if (mat[rowIter][colIter] != 0)
+                    cout << mat[rowIter][colIter] << " ";
+                else
+                    cout << "  ";
+                cout << "||";
+                cellIter = 1;
+            } else {
+                cout << " ";
+                if (mat[rowIter][colIter] != 0)
+                    cout << mat[rowIter][colIter] << "  ";
+                else
+                    cout << "   ";
+                cellIter++;
             }
-            cout<<endl;
         }
+
+        if (rowIter % 3 != 2) cout << "\n++-----------++-----------++-----------++";
+        else cout << "\n++=====================================++";
+    }
+
+    cout << " ";
+}
+
+/**
+  *	@desc The recursive function which does all the work, this iterates over the
+  *	possible values for the specified cell one-by-one. Once a value has been filled, it
+  *	goes to the next cell. Here, the same thing happens. If none of the possible values
+  *	work out, then the function backtracks to the previous cell.
+    *
+  *	@param mat (int [][]) 2d-matrix with input values to be solved
+  *	@param i (int) row of the current cell
+  *	@param j (int) col of the current cell
+  *	@param n (int) size of the board
+  *	@return (bool) whether the value put in the cell made it a SUCCESS or NOT
+*/
+bool solveSudoku(int mat[][9], int i, int j) {
+
+    //base case
+    if (i == 9) {
+        //prints the sudoku board
+        displayGrid(mat);
         return true;
     }
 
     //one column ends - shift to next row
-    if(j==n)
-        return solveSudoku(mat,i+1,0,n);
+    if (j == 9)
+        return solveSudoku(mat, i + 1, 0);
 
     //skipping pre-filled cells
-    if(mat[i][j]!=0)
-        return solveSudoku(mat,i,j+1,n);
+    if (mat[i][j] != 0)
+        return solveSudoku(mat, i, j + 1);
 
     //recursive case
-    for (int number = 1; number <=n ; ++number) {
-        if(canPlace(mat,i,j,n,number)){
+    //fills the current cell with possible option
+    for (int number = 1; number <= 9; ++number) {
+        if (canPlace(mat, i, j, number)) {
+            //assume
             mat[i][j] = number;
 
-            bool couldWeSolve = solveSudoku(mat,i,j+1,n);
+            bool couldWeSolve = solveSudoku(mat, i, j + 1);
 
-            if(couldWeSolve)
+            if (couldWeSolve)
                 return true;
-
         }
     }
-
+    //backtrack
     mat[i][j] = 0;
     return false;
 
 }
 
-int main(){
+int main() {
 
-    int n;
-    cin>>n;
+    cout << "\n======================\n";
+    cout << "    Sudoku Solver\n";
+    cout << "======================\n\n";
+
+    cout << "Welcome to Sudoku Solver!\n";
+    cout << "Before we start, you will have to input the puzzle to be solved.\n\n";
+    cout << "Note - Blank cells must be filled in as 0 (eg; 1 0 0  2 0 0 ...).\n\n";
 
     int arr[9][9];
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin>>arr[i][j];
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            cin >> arr[i][j];
         }
     }
 
-    solveSudoku(arr,0,0,n);
+    solveSudoku(arr, 0, 0);
 
-return 0;
+    return 0;
 }
-
-//9
-//5 3 0 0 7 0 0 0 0
-//6 0 0 1 9 5 0 0 0
-//0 9 8 0 0 0 0 6 0
-//8 0 0 0 6 0 0 0 3
-//4 0 0 8 0 3 0 0 1
-//7 0 0 0 2 0 0 0 6
-//0 6 0 0 0 0 2 8 0
-//0 0 0 4 1 9 0 0 5
-//0 0 0 0 8 0 0 7 9
